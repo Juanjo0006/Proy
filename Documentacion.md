ab5cc3764aa7374195ef954d0655211fcfdcf9ef
Juanjo <wjuanjo.0006@gmail.com>
2023-05-22 23:26:41 -0600

    Actual

--- Documentacion.md
+++ Documentacion.md
@@ -2,4 +2,11 @@ Nombre del Juego: Black Jack
 
 Esta documentación proporciona información sobre el juego de blackjack desarrollado en los módulos de esta carpeta. El juego permite a los usuarios jugar en la consola y guardar sus estadísticas de juego en archivos de texto.
 
-En cartas: 
+En Baraja.py: 
+Se encuentra la creación de la baraja con la cantidad de cartas que posee en su totalidad, además se crea la función de repartir cartas. 
+
+En Mano.py se realiza la metodología de la puntuación y la lógica del conteo de cartas. 
+
+En Jugo.py se realizan todas las funciones relacionadas al BlockJack. 
+
+En Main se proporciona el menú y el registro en el documento de texto con el objetivo de registrar todos los puntos obtenidos por el jugador. 
--- Juego.py
+++ Juego.py
@@ -2,20 +2,20 @@ import Baraja
 import Mano
 
 # Jugar al blackjack
-def jugar_blackjack():
+def jugar_blackjack(nombre_usuario=None):
     baraja_juego = Baraja.crear_baraja()
     mano_jugador = []
-    mano_crupier = []
+    mano_dealer = []
 
     # Repartir las primeras dos cartas
     Baraja.repartir_carta(baraja_juego, mano_jugador)
-    Baraja.repartir_carta(baraja_juego, mano_crupier)
+    Baraja.repartir_carta(baraja_juego, mano_dealer)
     Baraja.repartir_carta(baraja_juego, mano_jugador)
-    Baraja.repartir_carta(baraja_juego, mano_crupier)
+    Baraja.repartir_carta(baraja_juego, mano_dealer)
 
     # Mostrar las cartas iniciales
     print('Tu mano:', mano_jugador)
-    print('Carta del crupier:', mano_crupier[0])
+    print('Carta del dealermano_dealer:', mano_dealer[0])
 
     # Jugar la mano del jugador
     while True:
@@ -29,27 +29,27 @@ def jugar_blackjack():
         else:
             break
 
-    # Jugar la mano del crupier
-    while Mano.calcular_puntuacion(mano_crupier) < 17:
-        Baraja.repartir_carta(baraja_juego, mano_crupier)
+    # Jugar la mano del dealermano_dealer
+    while Mano.calcular_puntuacion(mano_dealer) < 17:
+        Baraja.repartir_carta(baraja_juego, mano_dealer)
 
     # Mostrar las manos finales
     print('Tu mano:', mano_jugador)
-    print('Carta del crupier:', mano_crupier)
+    print('Carta del dealermano_dealer:', mano_dealer)
 
     # Calcular puntuaciones
     puntuacion_jugador = Mano.calcular_puntuacion(mano_jugador)
-    puntuacion_crupier = Mano.calcular_puntuacion(mano_crupier)
+    puntuacion_dealermano_dealer = Mano.calcular_puntuacion(mano_dealer)
 
     # Determinar el resultado
     if puntuacion_jugador > 21:
         print('Te has pasado de 21. ¡Has perdido!')
-    elif puntuacion_crupier > 21:
-        print('El crupier se ha pasado de 21. ¡Has ganado!')
+    elif puntuacion_dealermano_dealer > 21:
+        print('El dealermano_dealer se ha pasado de 21. ¡Has ganado!')
     
-    elif puntuacion_jugador > puntuacion_crupier:
+    elif puntuacion_jugador > puntuacion_dealermano_dealer:
         print('¡Has ganado!')
-    elif puntuacion_crupier > puntuacion_jugador:
+    elif puntuacion_dealermano_dealer > puntuacion_jugador:
         print('Has perdido.')
     else: 
         print('Empate.')
